91fcd955ffc73b17495f9707665a94b46ff3c561
Raul Alvarez Gonzalez <ralvarez@clusterseven.com>
2017-03-09 18:02:02 +0000

    Adding some real content

--- raulalgo.js
+++ raulalgo.js
@@ -1,18 +1,44 @@
 $(document).ready(function () {
   // Using scrollstory
+
+  var counter = 0
+  var role_steps = ["english","spanish","abreviate","siglas"]
+
   $("#container").scrollStory({
-        contentSelector: '.story',
-        keyboard: true,
-        autoActivateFirstItem: true
+        contentSelector: '.story'
   });
 
-  // For using waypoint.js
+  $('#container').scrollStory({
+    contentSelector: '#red_box',
+    itementerviewport: function(ev, item) {
+      console.log("hola scrolly");
+    }
+  })
 
-  /*var waypoint = $('#basic_waypoint').waypoint({
-    handler: function(direction) {
-      console.log("hola waypoints");
-    },
-    offset: '100%'
-  })*/
+  /*$('#lead').click(function(){
+    $('#lead').toggleClass("anticipation rest").queue(function(){
+      $('#lead').toggleClass("anticipation rest")
+    });*/
+    //$('#far').toggleClass("away");
+    /*$('#lead').mousedown(function(){
+      $('#lead').addClass("anticipation");
+      $('#lead').removeClass("rest");
+    })*/
+    /*$('#lead').mouseup(function(){
+      $('#lead').removeClass("anticipation");
+      $('#lead').addClass("rest");
+    })*/
 
+    $('#role').click(function(){
+      console.log(counter);
+      if (counter == 0) {
+        $('#lead').toggleClass('away');
+        $('#english h1').toggleClass('relocate');
+
+      }
+      else{
+        $('#'+role_steps[counter-1]+', #' + role_steps[counter]).toggleClass("hide show")
+      }
+      counter++;
+    })
 });
